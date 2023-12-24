de59cbe03ffd0a0881040a2d5af9bfd053a4130b
Shinji Pons <accounts@shinjipons.com>
2023-12-24 17:46:53 +0100

    added chunking, embeddings and created vector store

--- app.py
+++ app.py
@@ -1,5 +1,39 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf) # creates a pdf object that has pages that you read from
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200, # each chunk will contain 200 characters from the previous chunk to not lose context/meaning
+        length_function=len # ???
+    )
+
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
 
 def main():
     load_dotenv()
@@ -10,8 +44,18 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"): # name must be unique
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create a vector store with the embeddings
+                # embeddings will be stored locally, in a real application those would need to be stored in the cloud
+                vectorstore = get_vectorstore(text_chunks)
 
 if __name__ == "__main__":
     main()
