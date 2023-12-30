1b32cb342447f821ed25b5c0866ce007bdac35d3
Shinji Pons <accounts@shinjipons.com>
2023-12-30 17:21:35 +0100

    Finished

--- app.py
+++ app.py
@@ -49,20 +49,35 @@ def get_conversation_chain(vectorstore):
     return conversation_chain
 
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
+    st.set_page_config(page_title="Chat with Multiple PDFs",
+                       page_icon=":books:")
 
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
-    st.header("Chat with Multiple PDFs")
-    st.text_input("Ask a question about your documents:")
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
-    st.write(user_template.replace("{{MSG}}", "Hello robot!"), unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "Hello human!"), unsafe_allow_html=True)
+    st.header("Chat with Multiple PDFs")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
