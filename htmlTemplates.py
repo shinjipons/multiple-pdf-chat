748b60ec8e818880bdde45c9a02a2efb7dd102ce
Shinji Pons <accounts@shinjipons.com>
2023-12-30 15:52:35 +0100

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,12 +1,38 @@
 css = """
 <style>
+.chat-message {
+    padding: 1.5rem;
+    border-radius: 0.5rem;
+    margin-bottom: 1rem;
+    display: flex;
+}
+.chat-message.user {
+    background-color: #2b313e;
+}
+.chat-message.bot {
+    background-color: #475063;
+}
+.chat-message .avatar {
+    width: 15%;
+}
+.chat-message .avatar img {
+    max-width: 78px;
+    max-height: 78px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+.chat-message .message {
+    width: 85%;
+    padding: 0 1.5rem;
+    color: #fff;
+}
 </style>
 """
 
 bot_template = """
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="" />
+        <img src="https://www.pngrepo.com/png/384670/512/account-avatar-profile-user.png" />
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -15,7 +41,7 @@ bot_template = """
 user_template = """
 <div class="chat-message user">
     <div class="avatar">
-        <img src="" />
+        <img src="https://www.pngrepo.com/png/384670/512/account-avatar-profile-user.png" />
     </div>
     <div class="message">{{MSG}}</div>
 </div>
