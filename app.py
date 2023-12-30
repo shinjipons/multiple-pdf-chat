a459e2010792095bc0e4f1209966f8aec8303024
Shinji Pons <accounts@shinjipons.com>
2023-12-30 15:36:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -35,12 +38,31 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
     st.header("Chat with Multiple PDFs")
     st.text_input("Ask a question about your documents:")
+
+    st.write(user_template.replace("{{MSG}}", "Hello robot!"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "Hello human!"), unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -56,6 +78,10 @@ def main():
                 # create a vector store with the embeddings
                 # embeddings will be stored locally, in a real application those would need to be stored in the cloud
                 vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                # using session_state will let streamlit know that this variable isn't supposed to be re-initialized
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 if __name__ == "__main__":
     main()
