96d025b9485542c2236491e9185e1303caede361
Shinji Pons <accounts@shinjipons.com>
2023-12-30 15:36:52 +0100

    Create htmlTemplates.py

---
+++ htmlTemplates.py
@@ -0,0 +1,22 @@
+css = """
+<style>
+</style>
+"""
+
+bot_template = """
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="" />
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+"""
+
+user_template = """
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="" />
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+"""
